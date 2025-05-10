cbcdc7ac940375fa837a2d065436cd53cb969e21
matsu <tmtmt2209@gmail.com>
2025-05-10 16:38:23 +0900

    chore: init

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "try-dlt-dbt";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pkgs.uv
+            pkgs.duckdb
+            pkgs.just
+          ];
+          shellHook = ''
+            uv venv
+            uv sync
+            source .venv/bin/activate
+          '';
+        };
+        formatter = pkgs.nixfmt-rfc-style;
+      }
+    );
+}
